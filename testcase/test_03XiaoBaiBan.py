e42aaf8f58188bccbe7b186343b4ef1edc0d2a73
apple <499800885@qq.com>
2018-09-12 15:29:48 +0800

    添加小白板删除评论脚本

--- testcase/test_03XiaoBaiBan.py
+++ testcase/test_03XiaoBaiBan.py
@@ -93,6 +93,44 @@ class TestXiaobaiban(unittest.TestCase):
         self.log.info_log("查看评论删除成功")
 
 
+    def test04RizhiAndShenpi(self):
+        self.log.info_log("查看小白板日志测试")
+        time.sleep(3)
+        self.d(text=u"我的小白板").click()
+        time.sleep(1)
+        self.d(text=u"冷藏库开辟市场参考").click()
+        time.sleep(1)
+        self.d(resourceId="com.esenyun.workline:id/title_iv_right02").click()
+        time.sleep(1)
+        self.d.swipe(0.729, 0.751,0.645, 0.326,0.5)
+        time.sleep(1)
+        self.d(text=u"查看日志").click() #进入查看小白板日志界面
+        time.sleep(1)
+        self.d.swipe(0.729, 0.751, 0.645, 0.326, 0.5)
+        time.sleep(1)
+        self.d.press("back")
+        time.sleep(1)
+        self.log.info_log("查看小白板日志测试完成")
+        time.sleep(1)
+        self.log.info_log("审批测试开始")
+        self.d(resourceId="com.esenyun.workline:id/title_iv_right02").click()
+        time.sleep(1)
+        self.d(text=u"发起审批").click()
+        time.sleep(3)
+        self.d.click(0.505, 0.847)
+        time.sleep(1)
+        self.d.click(0.513, 0.205)
+        time.sleep(3)
+        self.d(text=u"adsaf").click()
+        time.sleep(1)
+        self.d(resourceId="com.esenyun.workline:id/tv_confirm").click()
+        time.sleep(1)
+        self.d.click(0.561, 0.836)
+        time.sleep(1)
+        self.assertTrue(self.d(text=u"点赞").exists,"审批失败")
+        time.sleep(1)
+        self.log.info_log("审批测试通过")
+
 
 
 
